376e42a169f022adeb77cfed2c8c00d64a16805c
Brian David Nejand <david@>
2021-10-05 18:48:56 -0400

    Part 1 - Putting Shapes on the Screen

--- GameCode.swift
+++ GameCode.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+// Create an new instance of OvalShape type named circle with width and height of 150
+let circle = OvalShape(width:150, height: 150)
+
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -12,4 +15,11 @@ of a function.
 
 func setup() {
     
+    // create a new instance of Point and assign to position property of circle
+    // which defines its location on the screen using x and y coordinates
+    circle.position = Point(x:250, y: 400)
+    
+    // the scene instance represents the white area on the screen,
+    // using add method to put circle into the scene
+    scene.add(circle)
 }
